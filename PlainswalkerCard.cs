80ad15d1ba5e6ff85b8b085021c7e57be1e34531
Braber01 <ben.raber89@gmail.com>
2017-10-15 12:58:47 -0500

    Adde PlainswalkerCard.cs

---
+++ PlainswalkerCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnitityFrameWorkReWrite {
+    public class PlainswalkerCard {
+
+        private byte loyatly;
+
+        public byte Loyatly { get; set; }
+
+        public PlainswalkerCard() : base() {
+
+        }
+
+    }
+}
