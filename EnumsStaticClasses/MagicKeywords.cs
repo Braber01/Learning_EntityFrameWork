7803580096f6ad6d02bc3d1296e374324f6c70f7
Braber01 <ben.raber89@gmail.com>
2017-10-15 13:00:20 -0500

    Added EnumsStaticClasses/MagicKeywords.cs

---
+++ EnumsStaticClasses/MagicKeywords.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace EnitityFrameWorkReWrite.EnumsStaticClasses {
+    public static class MagicKeywords {
+        public static BigInteger NONE = 0;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static BigInteger DEATHTOUCH = 1 << 0;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static BigInteger DEFENDER = 1 << 1;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static BigInteger DOUBLE_STRIKE = 1 << 2;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static BigInteger ENCHANT = 1 << 3;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static BigInteger EQUIP = 1 << 4;
+
+
+        public static BigInteger FIRST_STRIKE = 1 << 5;
+
+        public static BigInteger FLASH = 1 << 6;
+
+        public static BigInteger FLYING = 1 << 7;
+
+        public static BigInteger HASTE = 1 << 8;
+
+        public static BigInteger HEXPROOF = 1 << 9;
+
+        public static BigInteger INDESTRUCTABLE = 1 << 10;
+
+        public static BigInteger LIFELINK = 1 << 11;
+
+        public static BigInteger MENACE = 1 << 12;
+
+        public static BigInteger PROWESS = 1 << 13;
+
+        public static BigInteger REACH = 1 << 14;
+
+        public static BigInteger SCRY = 1 << 15;
+
+        public static BigInteger TRAMPLE = 1 << 16;
+
+        public static BigInteger VIGLANCE = 1 << 17;
+
+        public static BigInteger ABSORB = 1 << 18;
+
+        public static BigInteger AFFINITY = 1 << 19;
+
+        public static BigInteger AFTERMATH = 1 << 20;
+
+        public static BigInteger AMPLIFY = 1 << 21;
+
+        public static BigInteger ANNIHILATOR = 1 << 22;
+
+        public static BigInteger AURA_SWAP = 1 << 23;
+
+        public static BigInteger BANDS_WITH_OTHER = 1 << 24;
+
+        public static BigInteger BATTLE_CRY = 1 << 25;
+
+        public static BigInteger BESTOW = 1 << 26;
+
+        public static BigInteger BOLSTER = 1 << 27;
+
+        public static BigInteger BLOODTHIRST = 1 << 28;
+
+        public static BigInteger BUSHIDO = 1 << 29;
+
+        public static BigInteger BUYBACK = 1 << 30;
+
+        public static BigInteger CASCADE = 1 << 31;
+
+        public static BigInteger CHAMPION = 1 << 32;
+
+        public static BigInteger CHANGELING = 1 << 33;
+
+        public static BigInteger CIPHER = 1 << 34;
+
+        public static BigInteger CLASH = 1 << 35;
+
+        public static BigInteger CONSPIRE = 1 << 36;
+
+        public static BigInteger CONVOKE = 1 << 37;
+
+        public static BigInteger CREW = 1 << 38;
+
+        public static BigInteger CUMULATIVE_UPKEEP = 1 << 39;
+
+        public static BigInteger CYCLING = 1 << 40;
+
+        public static BigInteger DASH = 1 << 41;
+
+        public static BigInteger DELVE = 1 << 42;
+
+        public static BigInteger DETAIN = 1 << 43;
+
+        public static BigInteger DEVOUR = 1 << 44;
+
+        public static BigInteger DREDGE = 1 << 45;
+
+        public static BigInteger ECHO = 1 << 46;
+
+        public static BigInteger EMBALM = 1 << 47;
+
+        public static BigInteger ENTWINE = 1 << 48;
+
+        public static BigInteger EPIC = 1 << 49;
+
+        public static BigInteger EVOLVE = 1 << 50;
+
+        public static BigInteger EVOKE = 1 << 51;
+
+        public static BigInteger EXALTED = 1 << 52;
+
+        public static BigInteger EXERT = 1 << 53;
+
+        public static BigInteger EXPLOIT = 1 << 54;
+
+        public static BigInteger EXTORT = 1 << 55;
+
+        public static BigInteger FADING = 1 << 56;
+
+        public static BigInteger FATESEAL = 1 << 57;
+
+        public static BigInteger FLANKING = 1 << 58;
+
+        public static BigInteger FLASHBACK = 1 << 59;
+
+        public static BigInteger FLIP = 1 << 60;
+
+        public static BigInteger FORCAST = 1 << 61;
+
+        public static BigInteger FORTIFY = 1 << 62;
+
+        public static BigInteger FRENZY = 1 << 63;
+
+        public static BigInteger GRAFT = 1 << 64;
+
+        public static BigInteger GRAVESTORM = 1 << 65;
+
+        public static BigInteger HAUNT = 1 << 66;
+
+        public static BigInteger HIDEAWAY = 1 << 67;
+
+        public static BigInteger HORSEMANSHIP = 1 << 68;
+
+        public static BigInteger INFECT = 1 << 69;
+
+        public static BigInteger KICKER = 1 << 70;
+
+        public static BigInteger LEVEL_UP = 1 << 71;
+
+        public static BigInteger LIVING_WEAPON = 1 << 72;
+
+        public static BigInteger MADNESS = 1 << 73;
+
+        public static BigInteger MANIFEST = 1 << 74;
+
+        public static BigInteger MELD = 1 << 75;
+
+        public static BigInteger MIRACLE = 1 << 76;
+
+        public static BigInteger MODULAR = 1 << 77;
+
+        public static BigInteger MONSTROSITY = 1 << 78;
+
+        public static BigInteger MORPH = 1 << 79;
+
+        public static BigInteger MULTIKICKER = 1 << 80;
+
+        public static BigInteger NINJUTSU = 1 << 81;
+
+        public static BigInteger OFFERING = 1 << 82;
+
+        public static BigInteger OVERLOAD = 1 << 83;
+
+        public static BigInteger PERSIST = 1 << 84;
+
+        public static BigInteger PHASING = 1 << 85;
+
+        public static BigInteger POISONOUS = 1 << 86;
+
+        public static BigInteger POPULATE = 1 << 87;
+
+        public static BigInteger PROLIFERATE = 1 << 88;
+
+        public static BigInteger PROVOKE = 1 << 89;
+
+        public static BigInteger PROWL = 1 << 90;
+
+        public static BigInteger RAMPAGE = 1 << 91;
+
+        public static BigInteger REBOUND = 1 << 92;
+
+        public static BigInteger RECOVER = 1 << 93;
+
+        public static BigInteger REINFORCE = 1 << 94;
+
+        public static BigInteger RENOWN = 1 << 95;
+
+        public static BigInteger REPLICATE = 1 << 96;
+
+        public static BigInteger RETRACE = 1 << 97;
+
+        public static BigInteger RIPPLE = 1 << 98;
+
+        public static BigInteger SCAVENGE = 1 << 99;
+
+        public static BigInteger SHADOW = 1 << 100;
+
+        public static BigInteger SOULBOUND = 1 << 101;
+
+        public static BigInteger SOULSHIFT = 1 << 102;
+
+        public static BigInteger SPLICE = 1 << 103;
+
+        public static BigInteger SPLIT_SECOND = 1 << 104;
+
+        public static BigInteger STORM = 1 << 105;
+
+        public static BigInteger SUNBURST = 1 << 106;
+
+        public static BigInteger SUSPEND = 1 << 107;
+
+        public static BigInteger TOTEM_ARMOR = 1 << 108;
+
+        public static BigInteger TRANSFIGURE = 1 << 109;
+
+        public static BigInteger TRANSFORM = 1 << 110;
+
+        public static BigInteger TRANSMUTE = 1 << 111;
+
+        public static BigInteger TYPECYCLING = 1 << 112;
+
+        public static BigInteger UNDYING = 1 << 113;
+
+        public static BigInteger UNEARTH = 1 << 114;
+
+        public static BigInteger UNLEASH = 1 << 115;
+
+        public static BigInteger VANISHING = 1 << 116;
+
+        public static BigInteger WITHER = 1 << 117;
+
+        public static BigInteger BATTALION = 1 << 118;
+
+        public static BigInteger BLOODRUSH = 1 << 119;
+
+        public static BigInteger CHANNEL = 1 << 120;
+
+        public static BigInteger CHROMA = 1 << 121;
+
+        public static BigInteger DOMAIN = 1 << 122;
+
+        public static BigInteger FATEFUL_HOUR = 1 << 123;
+
+        public static BigInteger FEROCIOUS = 1 << 124;
+
+        public static BigInteger GRANDEUR = 1 << 125;
+
+        public static BigInteger HELLBENT = 1 << 126;
+
+        public static BigInteger HEROIC = 1 << 127;
+
+        public static BigInteger JOIN_FORCES = 1 << 128;
+
+        public static BigInteger KINSHIP = 1 << 129;
+
+        public static BigInteger LANDFALL = 1 << 130;
+
+        public static BigInteger METALCRAFT = 1 << 131;
+
+        public static BigInteger MORBID = 1 << 132;
+
+        public static BigInteger RADIANCE = 1 << 133;
+
+        public static BigInteger RAID = 1 << 134;
+
+        public static BigInteger RALLY = 1 << 135;
+
+        public static BigInteger SWEEP = 1 << 136;
+
+        public static BigInteger THRESHOLD = 1 << 137;
+
+        public static BigInteger BANDING = 1 << 138;
+
+        public static BigInteger BURY = 1 << 139;
+
+        public static BigInteger FEAR = 1 << 140;
+
+        public static BigInteger INTIMIDATE = 1 << 141;
+
+
+
+
+    }
+}
