a555099f26a0dcc81bd7b8715628eb7a819e9c2d
Braber01 <ben.raber89@gmail.com>
2017-10-15 12:56:21 -0500

    Added CardCollection.cs

---
+++ CardCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnitityFrameWorkReWrite {
+    public class CardCollection {
+        public ICollection<MagicCard> Spells { get; set; }
+        public ICollection<CreatureCard> Creatures { get; set; }
+        public ICollection<PlainswalkerCard> Plainswalkers { get; set; }
+    }
+
+}
