cc5bf7347fe08a6e258ed3f8ee88a63f796ad27e
Braber01 <ben.raber89@gmail.com>
2017-10-15 12:57:47 -0500

    Added CreatureCard.cs

---
+++ CreatureCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using EnitityFrameWorkReWrite.EnumsStaticClasses;
+
+namespace EnitityFrameWorkReWrite {
+    public class CreatureCard : MagicCard {
+        private byte power;
+        private byte toughness;
+        private BigInteger creatureTypes;
+
+        public byte Power { get; set; }
+        public byte Toughness { get; set; }
+        public BigInteger CreatureTypes { get; set; }
+
+        public CreatureCard() : base() {
+
+        }
+
+    }
+}
