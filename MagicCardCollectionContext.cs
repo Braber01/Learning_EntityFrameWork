85906e5c3ccc34dd09e732b0b0916b095fd60483
Braber01 <ben.raber89@gmail.com>
2017-10-15 13:03:48 -0500

    Added MagicCardCollectionContext.cs

---
+++ MagicCardCollectionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnitityFrameWorkReWrite {
+    public class MagicCardCollectionContext : DbContext{
+
+        public MagicCardCollectionContext() : base() {
+
+        }
+
+        public DbSet<MagicCard> Spells { get; set; }
+        public DbSet<CreatureCard> Creatures { get; set; }
+        public DbSet<PlainswalkerCard> Plainswalkers { get; set; }
+    }
+}
